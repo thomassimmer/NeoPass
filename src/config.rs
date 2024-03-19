b6d488a7e53a07b7b91c6bce6c2c995168fb88bc
Thomas Simmer <thomas.simmer@hotmail.fr>
2024-03-19 20:55:20 +0100

    Refactoring

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,13 @@ pub const SYMBOLS_TO_USE_IN_PASSWORDS: &str =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!*#_-|&@~$";
 pub const FILE_PATH: &str = "passwords.txt";
 pub const CLEAR_SCREEN: &str = "\x1b[2J\x1b[1;1H";
-pub const ENTER_NEW_PASSWORD: &str = "Enter a new password:";
-pub const ENTER_PASSWORD: &str = "Enter your password:";
+pub const ENTER_NEW_PASSWORD: &str = "Enter a master password:";
+pub const ENTER_PASSWORD: &str = "Enter your master password:";
 pub const PASSWORD_COPIED: &str = "✅ Copied password to clipboard.\n";
 pub const INSTRUCTIONS: &str = "Use ↑ and ↓ arrows to navigate between entries.\n\
-Press 'a' to add an new entry.\n\
-Press 'd' or Del to delete an entry.\n\
+Press 'a' or 'A' to add an new entry.\n\
+Press 'm' or 'M' to modify an entry.\n\
+Press 'd', 'D' or Del to delete an entry.\n\
 Press Enter or Space to copy the password in your clipboard.\n\
 Use 'q' or Ctrl + C to quit.\n";
 pub const INVALID_PASSWORD: &str = "❌ Invalid password.";
--- src/utils.rs
+++ src/utils.rs
@@ -2,10 +2,16 @@ use ::rand::{thread_rng, Rng};
 use cocoon::Cocoon;
 use std::io::{Error, ErrorKind};
 use std::{error::Error as ErrorTrait, fs::File};
+use tabled::settings::object::Rows;
+use tabled::settings::{Alignment, Style};
+use tabled::Table;
 
 use dialoguer::{theme::ColorfulTheme, Input, Password};
 
-use crate::config::CLEAR_SCREEN;
+use crate::config::{
+    CHECKING_PASSWORD, CLEAR_SCREEN, ENTER_NEW_PASSWORD, ENTER_PASSWORD, INVALID_PASSWORD,
+    NO_PASSWORD,
+};
 use crate::{
     config::{FILE_PATH, PASSWORD_LENGTH, SYMBOLS_TO_USE_IN_PASSWORDS},
     entry::Entry,
@@ -143,7 +149,7 @@ pub fn modify_entry(entry: &Entry) -> Entry {
     }
 }
 
-pub fn display_end_of_table(rows: Vec<&str>) {
+pub fn display_end_of_table(rows: Vec<String>) {
     // Print the rest of the table. It's cleaner.
     for row in &rows {
         println!("  {}", row);
@@ -154,4 +160,73 @@ pub fn display_end_of_table(rows: Vec<&str>) {
 pub fn clear_screen() {
     // Clean and get cursor back on top.
     print!("{}", CLEAR_SCREEN);
+}
+
+pub fn get_user_password(
+    entries: &mut Vec<Entry>,
+    password: &mut String,
+) -> Result<(), Box<dyn ErrorTrait>> {
+    // Ask the user for a password
+    let mut password_is_correct = false;
+
+    while !password_is_correct {
+        *password = String::new();
+
+        if File::open(FILE_PATH).is_ok() {
+            println!("{}", ENTER_PASSWORD);
+        } else {
+            println!("{}", ENTER_NEW_PASSWORD);
+        }
+
+        std::io::stdin().read_line(password)?;
+
+        println!("{}", CHECKING_PASSWORD);
+
+        clear_screen();
+
+        match decrypt_file(&password) {
+            Ok(found_entries) => {
+                password_is_correct = true;
+                *entries = found_entries;
+            }
+            Err(_) => {
+                println!("{}", INVALID_PASSWORD);
+                continue;
+            }
+        };
+    }
+    Ok(())
+}
+
+pub fn add_first_entry(
+    entries: &mut Vec<Entry>,
+    password: &mut String,
+) -> Result<(), Box<dyn ErrorTrait>> {
+    println!("{}", NO_PASSWORD);
+
+    entries.push(add_a_new_entry());
+
+    write_entries_in_file(&entries, &password)?;
+    Ok(())
+}
+
+pub fn build_rows(entries: &Vec<Entry>) -> Vec<String> {
+    // Build table.
+    let mut table = Table::new(entries.iter().map(|e| Entry {
+        application: e.application.clone(),
+        username: e.username.clone(),
+        password: "********".to_string(),
+    }));
+    let table = table
+        .with(Style::rounded())
+        .modify(Rows::new(1..), Alignment::left());
+
+    let table_as_string = table.to_string();
+
+    // Get table rows so we can make them selectable.
+    let rows: Vec<String> = table_as_string
+        .split('\n')
+        .map(|e| e.into())
+        .collect::<Vec<String>>();
+    rows
 }
